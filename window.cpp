caa319ee730ffa7c45f254594441d57e19f68117
Xienex <keisen097@gmail.com>
2016-07-20 18:53:33 +0200

    Initial commit

---
+++ window.cpp
@@ -0,0 +1,47 @@
+#include "window.h"
+
+Window::Window(int32_t width, int32_t height, const char *title)
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+    this->window = glfwCreateWindow(width, height, title, NULL, NULL);
+    this->width = width;
+    this->height = height;
+    this->title = title;
+
+    if(!this->window)
+        return;
+
+    glfwMakeContextCurrent(this->window);
+
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK)
+        return;
+
+    int w, h;
+    glfwGetFramebufferSize(this->window, &w, &h);
+    glViewport(0, 0, w, h);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);	
+}
+
+Window::~Window()
+{
+    glfwTerminate();
+}
+
+int Window::shouldClose()
+{
+    return glfwWindowShouldClose(this->window);
+}
+
+void Window::render()
+{
+    glfwSwapBuffers(this->window);
+    glfwPollEvents();
+}
---
+++ window.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdint.h>
+#include <string>
+
+class Window
+{
+public:
+    Window(int32_t, int32_t, const char *);
+    ~Window();
+
+    int shouldClose();
+    void render();
+
+    inline GLFWwindow *getWindow() { return window; };
+private:
+    GLFWwindow *window;
+    int32_t width;
+    int32_t height;
+    const char *title;
+};
